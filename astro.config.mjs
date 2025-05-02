43951603c424a8853379b4dd12d43e83468658bd
polymutex <polymutex@protonmail.com>
2025-05-02 15:25:43 +0800

    Produce static site build, and enforce that this remains the case.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
 	base: process.env.BASE_URL ?? '/',
-  site: process.env.SITE_URL ?? 'https://wallet.page', // Set your production site URL here
-  integrations: [
-    react(),
-    sitemap(), // Adds sitemap integration
-  ],
-  vite: {
-    ssr: {
-      noExternal: [
-        '@mui/*',
-      ],
-      external: [
-        '@mui/x-internals',
-      ],
-    },
-  },
-});
+	site: process.env.SITE_URL ?? 'https://wallet.page', // Set your production site URL here
+	output: 'static',
+	integrations: [
+		react(),
+		sitemap(), // Adds sitemap integration
+	],
+	vite: {
+		ssr: {
+			noExternal: ['@mui/*'],
+			external: ['@mui/x-internals'],
+		},
+	},
+})
