689550db7679c2dbdeb87099c0f6777a7d9a5936
Darryl Yeo <dev@darryl-yeo.com>
2025-05-20 17:53:04 -0700

    <Typography>: support Markdown parsing

--- src/ui/atoms/Typography.svelte
+++ src/ui/atoms/Typography.svelte
@@ -17,12 +17,47 @@
 		content: Content
 		strings?: Content extends TypographicContent<infer Strings> ? Strings : undefined
 	} = $props()
+
+
+	// Functions
+	import { marked } from 'marked'
+
+	marked.setOptions({
+		gfm: false,
+	})
 </script>
 
 
 {#if content.contentType === ContentType.TEXT}
 	{strings ? renderStrings(content.text, strings) : content.text}
 
-<!-- {:else if content.contentType === ContentType.MARKDOWN}
-	{content.markdown} -->
+{:else if content.contentType === ContentType.MARKDOWN}
+	{@const text = strings ? renderStrings(content.markdown, strings) : content.markdown}
+
+	<div class="markdown">
+		{#await marked.parse(text, {})}
+			...
+		{:then html}
+			{@html html}
+		{/await}
+	</div>
 {/if}
+
+
+<style>
+	.markdown {
+		display: grid;
+		gap: 1.25em;
+
+		:global {
+			ul, ol {
+				list-style-type: revert;
+				padding-inline-start: 1.5em;
+
+				li + li {
+					margin-top: 1em;
+				}
+			}
+		}
+	}
+</style>
