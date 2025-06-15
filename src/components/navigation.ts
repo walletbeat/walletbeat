0abaa54130e62f522a954bde914a8b784781a770
Darryl Yeo <dev@darryl-yeo.com>
2025-06-15 16:35:14 -0700

    <Navigation>: render nested navigation items

--- src/components/navigation.ts
+++ src/components/navigation.ts
@@ -1,10 +1,11 @@
-// Types
-export interface NavigationLinkItem {
+// Types/constants
+export type NavigationItem = {
 	id: string
 	title: string
-	icon: string
-	href: string
-	children?: NavigationLinkItem[]
+	icon?: string
+	href?: string
+	children?: NavigationItem[]
+	defaultIsCollapsed?: boolean
 }
 
 
@@ -17,42 +18,42 @@ import GithubIcon from 'lucide-static/icons/github.svg?raw'
 import MessageCircleIcon from 'lucide-static/icons/message-circle.svg?raw'
 
 
-export const navigationHome: NavigationLinkItem = {
+export const navigationHome: NavigationItem = {
 	id: 'wallet-table',
 	icon: HomeIcon,
 	title: 'Walletbeat',
 	href: '/#',
 }
 
-export const navigationFaq: NavigationLinkItem = {
+export const navigationFaq: NavigationItem = {
 	id: 'faq',
 	icon: HelpCircleIcon,
 	title: 'FAQ',
 	href: '/faq',
 }
 
-export const navigationAbout: NavigationLinkItem = {
+export const navigationAbout: NavigationItem = {
 	id: 'about',
 	icon: BuildingIcon,
 	title: 'About Walletbeat',
 	href: '/about',
 }
 
-export const navigationCriteria: NavigationLinkItem = {
+export const navigationCriteria: NavigationItem = {
 	id: 'criteria',
 	icon: BadgeCheckIcon,
 	title: 'Evaluation Criteria',
 	href: '/#criteria',
 }
 
-export const navigationRepository: NavigationLinkItem = {
+export const navigationRepository: NavigationItem = {
 	id: 'code-repository',
 	icon: GithubIcon,
 	title: 'Contribute on GitHub',
 	href: 'https://github.com/walletbeat/walletbeat',
 }
 
-export const navigationFarcasterChannel: NavigationLinkItem = {
+export const navigationFarcasterChannel: NavigationItem = {
 	id: 'farcaster-channel',
 	icon: MessageCircleIcon,
 	title: 'Discuss on Farcaster',
