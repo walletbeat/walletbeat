8fc1739af59b284366f47f6e549638f0805f460b
Darryl Yeo <dev@darryl-yeo.com>
2025-06-22 03:55:11 -0700

    React: delete!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { resolve } from 'node:path'
 
-import react from '@astrojs/react'
 import svelte from '@astrojs/svelte'
 import sitemap from '@astrojs/sitemap'
 import { shield } from '@kindspells/astro-shield'
@@ -16,7 +15,6 @@ export default defineConfig({
 	site: process.env.SITE_URL ?? 'https://wallet.page', // Set your production site URL here
 	output: 'static',
 	integrations: [
-		react(),
 		svelte(),
 		sitemap(),
 		shield({
@@ -27,25 +25,9 @@ export default defineConfig({
 		}),
 	],
 	vite: {
-		optimizeDeps: {
-			exclude: ['@mui/x-internals'],
-		},
 		build: {
 			// Improve chunking strategy
 			chunkSizeWarningLimit: 1000,
-			rollupOptions: {
-				output: {
-					manualChunks: {
-						react: ['react', 'react-dom'],
-						mui: ['@mui/material', '@mui/icons-material'],
-						reactIcons: ['react-icons'],
-					},
-				},
-			},
-		},
-		ssr: {
-			noExternal: ['@mui/*'],
-			external: ['@mui/x-internals'],
 		},
 	},
 })
