36c6ad9a59b8ad643e850e86904e1c2ecda82799
maykelxyz <michaelpediglorio1109@gmail.com>
2025-11-13 22:04:33 +0800

    fix: add signingIntentClarity in embedded

--- data/embedded-wallets/unrated.tmpl.ts
+++ data/embedded-wallets/unrated.tmpl.ts
@@ -53,6 +53,7 @@ export const unratedEmbeddedTemplate: EmbeddedWallet = {
 			},
 			passkeyVerification: null,
 			publicSecurityAudits: null,
+			signingIntentClarity: null,
 		},
 		selfSovereignty: {
 			interoperability: null,
