171d620533093363dfe9c9853eccff8858a4e27e
maykelxyz <michaelpediglorio1109@gmail.com>
2025-12-11 19:41:41 +0800

    Chore: Add 0xmattmatt as contributor

---
+++ data/contributors/0xmattmatt.ts
@@ -0,0 +1,7 @@
+import type { Contributor } from '@/schema/wallet'
+
+export const mattmatt: Contributor = {
+	name: '0xmattmatt',
+	affiliation: 'NO_AFFILIATION',
+	url: 'https://x.com/0xmattmatt',
+}
