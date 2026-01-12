f64921fc3f3612ef21458eb2024cdab1b42fa730
Darryl Yeo <dev@darryl-yeo.com>
2026-01-12 09:03:33 +0000

    Prettier: format

--- data/news/index.ts
+++ data/news/index.ts
@@ -4,12 +4,7 @@ import type { WalletSecurityNews } from '@/types/content/news'
  * All news articles about wallet security incidents, sorted by date (newest first)
  * Compiled from individual news files
  */
-export const allWalletSecurityNews: WalletSecurityNews[] = (
-	[
-		(await import('./2025-12-25-browser-extension-v268-incident')).default,
-		(await import('./2026-01-06-global-e-breach')).default,
-	]
-		.sort((a, b) => (
-			b.publishedAt.localeCompare(a.publishedAt)
-		))
-)
+export const allWalletSecurityNews: WalletSecurityNews[] = [
+	(await import('./2025-12-25-browser-extension-v268-incident')).default,
+	(await import('./2026-01-06-global-e-breach')).default,
+].sort((a, b) => b.publishedAt.localeCompare(a.publishedAt))
