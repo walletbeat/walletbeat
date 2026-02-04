8d621e4c1fb9a7f438ee40d3a28f8e5c17efcc1c
maykelxyz <michaelpediglorio1109@gmail.com>
2026-02-04 22:58:33 +0800

    Chore: Add natspec comments

--- resources/contracts/src/WalletbeatTestErc20.sol
+++ resources/contracts/src/WalletbeatTestErc20.sol
@@ -4,16 +4,34 @@ pragma solidity 0.8.24;
 import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
+/**
+ * @title WalletbeatTestErc20
+ * @author Walletbeat
+ * @notice A test ERC20 token used to evaluate how wallets simulate and display token transactions
+ * @dev This token is soulbound and can only be minted or burned by the owner
+ */
 contract WalletbeatTestErc20 is ERC20, Ownable {
     error WalletbeatTestErc20__Soulbound();
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
+    /**
+     * @notice Mints a variable amount of tokens to the specified user
+     * @dev The amount minted is determined by `1 + (block.number % 100)` to introduce
+     * unpredictability in transaction simulations.
+     * @param user The address to receive the minted tokens
+     */
     function mint(address user) external onlyOwner {
         uint256 tokensToMint = 1 + (block.number % 100);
         super._mint(user, tokensToMint);
     }
 
+    /**
+     * @notice Replicates a malicious claim function that burns a user's tokens
+     * @dev Burns either the user's entire balance or 1 token depending on block number parity.
+     * This simulates unpredictable drain behavior that wallets should detect.
+     * @param user The address whose tokens will be burned
+     */
     function claim(address user) external onlyOwner {
         uint256 userBalance = balanceOf(user);
         if (block.number % 2 == 0) {
@@ -23,11 +41,21 @@ contract WalletbeatTestErc20 is ERC20, Ownable {
         }
     }
 
+    /**
+     * @notice Burns all tokens held by the specified user
+     * @dev This function drains the user's entire token balance.
+     * @param user The address whose tokens will be burned
+     */
     function burn(address user) external onlyOwner {
         uint256 userBalance = balanceOf(user);
         _burn(user, userBalance);
     }
 
+    /**
+     * @notice Enforces soulbound behavior by preventing token transfers
+     * @dev Allows minting (from == address(0)) and burning (to == address(0)) but reverts
+     * on any other transfer attempt.
+     */
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
         super._beforeTokenTransfer(from, to, amount);
 
