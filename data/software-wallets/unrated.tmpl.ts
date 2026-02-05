d8eb6dcee31b25ab0c8fb74d308a817081d954e1
maykelxyz <michaelpediglorio1109@gmail.com>
2026-02-06 04:50:17 +0800

    Chore: pnpm lint

--- data/software-wallets/unrated.tmpl.ts
+++ data/software-wallets/unrated.tmpl.ts
@@ -1,9 +1,5 @@
 import { exampleContributor } from '@/data/contributors/example'
 import { WalletProfile } from '@/schema/features/profile'
-import {
-	DataDisplayOptions,
-	MessageSigningDetails,
-} from '@/schema/features/security/transaction-legibility'
 import { TransactionSubmissionL2Type } from '@/schema/features/self-sovereignty/transaction-submission'
 import { refTodo } from '@/schema/reference'
 import { Variant } from '@/schema/variants'
