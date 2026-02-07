e5b8e8bdab52051cc8de36eb4dfdfe58ba1664f1
maykelxyz <michaelpediglorio1109@gmail.com>
2026-02-07 21:08:38 +0800

    Tests: Add tests for deployment

--- resources/contracts/test/WalletbeatDeploymentTest.t.sol
+++ resources/contracts/test/WalletbeatDeploymentTest.t.sol
@@ -17,4 +17,32 @@ contract WalletbeatDeploymentTest is Test {
         deployer = new DeployContract();
         (tc, erc20, erc721) = deployer.run();
     }
+
+    function testErc20OwnerIsTestContract() external view {
+        assertEq(erc20.owner(), address(tc));
+    }
+
+    function testErc721OwnerIsTestContract() external view {
+        assertEq(erc721.owner(), address(tc));
+    }
+
+    function testErc20Name() external view {
+        assertEq(erc20.name(), "Walletbeat Testing ERC20");
+    }
+
+    function testErc721Name() external view {
+        assertEq(erc721.name(), "Walletbeat Testing ERC721");
+    }
+
+    function testErc20Symbol() external view {
+        assertEq(erc20.symbol(), "WBT");
+    }
+
+    function testErc721Symbol() external view {
+        assertEq(erc721.symbol(), "WBT");
+    }
+
+    function testFakeTokensToClaimConstant() external view {
+        assertEq(tc.FAKE_TOKENS_TO_CLAIM(), 1e18);
+    }
 }
