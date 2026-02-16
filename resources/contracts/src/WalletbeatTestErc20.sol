1807ae86e9e2e09f8703dcd04f9762e040b5044c
maykelxyz <michaelpediglorio1109@gmail.com>
2026-02-16 14:36:22 +0800

    Chore: imprve natspec

--- resources/contracts/src/WalletbeatTestErc20.sol
+++ resources/contracts/src/WalletbeatTestErc20.sol
@@ -7,7 +7,8 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * @title WalletbeatTestErc20
  * @author Walletbeat
  * @notice A test ERC20 token used to evaluate how wallets simulate and display token transactions
- * @dev This token is soulbound and can only be minted or burned by the owner
+ * @dev This token is soulbound and can be minted or burned by anyone. This is intentional
+ * to test how wallets handle permissionless token manipulation.
  */
 contract WalletbeatTestErc20 is ERC20 {
     error WalletbeatTestErc20__Soulbound();
@@ -18,7 +19,7 @@ contract WalletbeatTestErc20 is ERC20 {
      * @notice Mints a variable amount of tokens to the specified user
      * @dev The amount minted is determined by `1 + (block.number % 100)` to introduce
      * unpredictability in transaction simulations.
-     * Anyone can mint to other addresses
+     * Anyone can mint to any address. This is intentional to allow unrestricted testing.
      * @param user The address to receive the minted tokens
      */
     function mint(address user) external {
@@ -30,7 +31,8 @@ contract WalletbeatTestErc20 is ERC20 {
      * @notice Replicates a malicious claim function with unpredictable behavior
      * @dev Mints tokens if block.number is even, burns the user's entire balance if odd.
      * This simulates unpredictable drain behavior that wallets should detect.
-     * Anyone can claim using other address
+     * Anyone can call this on behalf of any address. This is intentional to simulate
+     * malicious contracts that manipulate other users' balances.
      * @param user The address to receive minted tokens or have their balance burned
      */
     function claim(address user) external {
@@ -46,7 +48,8 @@ contract WalletbeatTestErc20 is ERC20 {
     /**
      * @notice Burns all tokens held by the specified user
      * @dev This function drains the user's entire token balance.
-     * Anyone can burn other address' tokens
+     * Anyone can burn any address' tokens. This is intentional to simulate
+     * malicious drain behavior that wallets should detect and warn about.
      * @param user The address whose tokens will be burned
      */
     function burn(address user) external {
