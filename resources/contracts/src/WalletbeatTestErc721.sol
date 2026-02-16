70d62847a160c27a1a52af9fd06e1014ae9e22cb
maykelxyz <michaelpediglorio1109@gmail.com>
2026-02-16 14:12:45 +0800

    Chore: add natspec comments to ERC712 contract

--- resources/contracts/src/WalletbeatTestErc721.sol
+++ resources/contracts/src/WalletbeatTestErc721.sol
@@ -3,6 +3,12 @@ pragma solidity 0.8.24;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+/**
+ * @title WalletbeatTestErc721
+ * @author Walletbeat
+ * @notice A test ERC721 token used to evaluate how wallets simulate and display NFT transactions
+ * @dev This token is soulbound and can only be minted or burned. Transfers between non-zero addresses revert.
+ */
 contract WalletbeatTestErc721 is ERC721 {
     error WalletbeatTestErc721__Soulbound();
 
@@ -10,6 +16,13 @@ contract WalletbeatTestErc721 is ERC721 {
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
 
+    /**
+     * @notice Mints a variable number of NFTs to the specified receiver
+     * @dev The amount minted is determined by `1 + (block.number % 4)` to introduce
+     * unpredictability in transaction simulations.
+     * Anyone can mint to any address.
+     * @param receiver The address to receive the minted NFTs
+     */
     function mint(address receiver) external {
         uint256 tokensToMint = 1 + (block.number % 4);
         for (uint256 i = 0; i < tokensToMint; i++) {
@@ -18,6 +31,11 @@ contract WalletbeatTestErc721 is ERC721 {
         }
     }
 
+    /**
+     * @notice Enforces soulbound behavior by preventing token transfers
+     * @dev Allows minting (from == address(0)) and burning (to == address(0)) but reverts
+     * on any other transfer attempt.
+     */
     function _beforeTokenTransfer(address from, address to, uint256 firstTokenId, uint256 batchSize)
         internal
         virtual
