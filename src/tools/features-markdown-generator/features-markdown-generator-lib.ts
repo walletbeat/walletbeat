495d10727ee054075b5062b841a0b58a7897e46a
maykelxyz <michaelpediglorio1109@gmail.com>
2026-03-03 04:29:39 +0800

    Fix: build issues

--- src/tools/features-markdown-generator/features-markdown-generator-lib.ts
+++ src/tools/features-markdown-generator/features-markdown-generator-lib.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs'
 import * as path from 'path'
-import * as ts from 'typescript'
+import ts from 'typescript'
 
 export interface FeaturesMarkdownConfig {
 	featuresSrcFile: string
