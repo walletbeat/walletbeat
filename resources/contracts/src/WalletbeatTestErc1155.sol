65c3db38759f493817b4217848cc048800a021e5
maykelxyz <michaelpediglorio1109@gmail.com>
2026-03-25 01:24:52 +0800

    Chore: Deploy contract

--- resources/contracts/src/WalletbeatTestErc1155.sol
+++ resources/contracts/src/WalletbeatTestErc1155.sol
@@ -16,9 +16,27 @@ contract WalletbeatTestErc1155 is ERC1155 {
 
     uint256 private s_tokenId;
     string private s_name;
+    string[] private s_imageUriChunks;
 
-    constructor(string memory imageUri, string memory name) ERC1155(imageUri) {
+    constructor(string memory name) ERC1155("") {
         s_name = name;
+    }
+
+    function setImageUri(string memory imageUri) external {
+        delete s_imageUriChunks;
+        s_imageUriChunks.push(imageUri);
+    }
+
+    function appendImageUri(string memory chunk) external {
+        s_imageUriChunks.push(chunk);
+    }
+
+    function _getImageUri() private view returns (string memory) {
+        bytes memory result;
+        for (uint256 i = 0; i < s_imageUriChunks.length; i++) {
+            result = bytes.concat(result, bytes(s_imageUriChunks[i]));
+        }
+        return string(result);
     }
 
     /**
@@ -84,7 +102,7 @@ contract WalletbeatTestErc1155 is ERC1155 {
                             s_name,
                             '", "description":"A test ERC1155 token used solely for testing.", ',
                             '"attributes": [{"trait_type": "purpose", "value": "testing"}], "image":"',
-                            super.uri(tokenId),
+                            _getImageUri(),
                             '"}'
                         )
                     )
