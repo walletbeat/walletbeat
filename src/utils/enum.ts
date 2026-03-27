1484c8a635d89932b6bd6523a23d73616acc0c8e
maykelxyz <michaelpediglorio1109@gmail.com>
2026-03-27 18:24:29 +0800

    Chore: remove unused func

--- src/utils/enum.ts
+++ src/utils/enum.ts
@@ -150,13 +150,3 @@ export function excludeFromEnum<E extends string, Excluded extends E>(
 		),
 	)
 }
-
-export function enumKey<T extends Record<string, string>>(enumObj: T, value: string): string {
-	for (const k in enumObj) {
-		if (enumObj[k] === value) {
-			return k
-		}
-	}
-
-	return value
-}
