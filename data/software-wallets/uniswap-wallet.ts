b4717dd17b00c43718987fe2ccd146d07ca426b6
johngrantuk <johngrantuk@googlemail.com>
2026-04-28 09:45:32 +0100

    Update missing wallets

--- data/software-wallets/uniswap-wallet.ts
+++ data/software-wallets/uniswap-wallet.ts
@@ -116,6 +116,15 @@ export const uniswapWallet: SoftwareWallet = {
 		},
 		transparency: {
 			operationFees: null,
+			releaseTransparency: {
+				artifactSigning: null,
+				dependencyLocking: null,
+				dependencyVulnerabilityScanning: null,
+				hasPublicChangelog: null,
+				hermeticBuilds: null,
+				repositoryChangeControls: null,
+				reproducibleBuilds: null,
+			},
 		},
 	},
 	variants: {
