ab4ee6ea9081418fffc0a334c74445aa994c729f
maykelxyz <michaelpediglorio1109@gmail.com>
2026-05-20 12:19:15 +0800

    News: Create bankrbot hack

--- data/news/index.ts
+++ data/news/index.ts
@@ -7,6 +7,7 @@ import type { WalletSecurityNews } from '@/types/content/news'
 export const allWalletSecurityNews: WalletSecurityNews[] = [
 	(await import('./2025-12-25-browser-extension-v268-incident')).default,
 	(await import('./2026-01-06-global-e-breach')).default,
+	(await import('./2026-05-20-bankrbot-hack')).default,
 ].sort((a, b) => b.publishedAt.localeCompare(a.publishedAt))
 
 /**
