12407ea4acfec96d3c273033aaa19078ed3b50c4
Darryl Yeo <dev@darryl-yeo.com>
2026-06-06 23:41:14 -0700

    Astro: use `<Font>` to load fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { resolve } from 'node:path'
 import sitemap from '@astrojs/sitemap'
 import svelte from '@astrojs/svelte'
 import { shield } from '@kindspells/astro-shield'
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 
 const rootDir = new URL('.', import.meta.url).pathname
 const modulePath = resolve(rootDir, 'src', 'generated', 'sriHashes.mjs')
@@ -33,4 +33,51 @@ export default defineConfig({
 			chunkSizeWarningLimit: 1000,
 		},
 	},
+	fonts: [
+		{
+			name: 'Avenir LT Std',
+			cssVariable: '--fontFamily-avenir',
+			provider: fontProviders.local(),
+			options: {
+				variants: [
+					{
+						src: ['./src/assets/fonts/avenir-lt-std-roman.otf'],
+						weight: '400',
+						style: 'normal',
+					},
+					{
+						src: ['./src/assets/fonts/avenir-lt-std-heavy.otf'],
+						weight: '700',
+						style: 'normal',
+					},
+				],
+			},
+		},
+		{
+			name: 'Space Grotesk',
+			cssVariable: '--fontFamily-spaceGrotesk',
+			provider: fontProviders.fontsource(),
+			weights: [400, 500, 600, 700],
+			styles: ['normal'],
+		},
+		{
+			name: 'SP Monorium',
+			cssVariable: '--fontFamily-spMonorium',
+			provider: fontProviders.local(),
+			fallbacks: ['monospace'],
+			options: {
+				variants: [
+					{
+						src: [
+							'./src/assets/fonts/sp-monorium-regular.woff2',
+							'./src/assets/fonts/sp-monorium-regular.woff',
+							'./src/assets/fonts/sp-monorium-regular.ttf',
+						],
+						weight: '400',
+						style: 'normal',
+					},
+				],
+			},
+		},
+	],
 })
