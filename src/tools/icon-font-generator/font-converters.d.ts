feb56e5fc3af4c11bae382858d804a87339381c5
Darryl Yeo <dev@darryl-yeo.com>
2026-06-26 02:15:58 -0700

    Icons: type font converter imports

---
+++ src/tools/icon-font-generator/font-converters.d.ts
@@ -0,0 +1,7 @@
+declare module 'ttf2eot' {
+	export default function ttf2eot(buffer: Buffer): Uint8Array
+}
+
+declare module 'ttf2woff' {
+	export default function ttf2woff(buffer: Buffer): Uint8Array
+}
