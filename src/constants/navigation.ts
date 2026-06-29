3734e3fa4bcc01f82aea69016e746adf8c2e1b73
Ifeoluwa <pheobeayo@gmail.com>
2026-06-29 09:18:53 +0100

    added to navigation page

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -69,6 +69,13 @@ export const navigationNews = {
 	href: '/news',
 } as const satisfies NavigationItem
 
+export const navigationWalletEips = {
+	id: 'wallet-eips',
+	icon: 'transaction_legibility',
+	title: 'Wallet EIPs',
+	href: '/wallet-eips/',
+} as const satisfies NavigationItem
+
 export const topbarNavigationItems = [
 	navigationAbout,
 	navigationFaq,
@@ -152,5 +159,6 @@ export const defaultNavigationItems = [
 		],
 	},
 	navigationNews,
+	navigationWalletEips,
 	navigationTesting,
 ] as const satisfies NavigationItem[]
