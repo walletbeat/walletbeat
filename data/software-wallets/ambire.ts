fb5ddd90e563fed647ea4a7c289566a6da544e94
polymutex <polymutex@protonmail.com>
2026-07-13 18:56:48 +0800

    Clean up file references to be under the same hierarchy.

--- data/software-wallets/ambire.ts
+++ data/software-wallets/ambire.ts
@@ -803,11 +803,11 @@ export const ambire: SoftwareWallet = {
 			transactionLegibility: {
 				ref: [
 					{
-						file: 'public/images/references/ambire/transaction_legibility_1.png',
+						file: 'public/references/wallets/ambire/screenshots/2026-06-11-transaction_legibility_1.png',
 						label: 'Transaction legibility screenshot 1',
 					},
 					{
-						file: 'public/images/references/ambire/transaction_legibility_2.png',
+						file: 'public/references/wallets/ambire/screenshots/2026-06-11-transaction_legibility_2.png',
 						label: 'Transaction legibility screenshot 2',
 					},
 				],
