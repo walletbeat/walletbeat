4882e973d16bddc25ee9ae8f19622bd585310165
polymutex <polymutex@protonmail.com>
2026-08-17 08:31:20 +0800

    ENS update: Try multiple RPC URLs in rotation.

--- deploy/update-ens.sh
+++ deploy/update-ens.sh
@@ -20,9 +20,26 @@ if [[ -z "${OMNIPIN_PK:-}" ]]; then
 	exit 1
 fi
 
+RPC_URLS=(
+	'https://public.1rpc.io/eth'
+	'https://ethereum-rpc.publicnode.com'
+	'https://rpc.flashbots.net'
+	'https://0xrpc.io/eth'
+	'https://eth.drpc.org'
+	'https://eth.blockrazor.xyz'
+	'https://rpc.mevblocker.io'
+)
+
 DIRECTORY_CID="$(pnpm omnipin pack --only-hash "$DEPLOY_DIRECTORY")"
-if [[ "${SKIP_HELIOS:-false}" == true ]]; then
-	exec pnpm omnipin ens "$DIRECTORY_CID" "$ENS_DOMAIN"
-else
-	exec pnpm helios:wrap pnpm omnipin ens --rpc-url='$HELIOS_RPC_ENDPOINT' "$DIRECTORY_CID" "$ENS_DOMAIN"
-fi
+while IFS= read -r RPC_URL; do
+	echo "Trying RPC URL: ${RPC_URL}..."
+	if [[ "${SKIP_HELIOS:-false}" == true ]]; then
+		if pnpm omnipin ens --rpc-url "$RPC_URL" "$DIRECTORY_CID" "$ENS_DOMAIN"; then
+			exit 0
+		fi
+	else
+		if ETHEREUM_MAINNET_EXECUTION_RPC_ENDPOINT="$RPC_URL" pnpm helios:wrap pnpm omnipin ens --rpc-url='$HELIOS_RPC_ENDPOINT' "$DIRECTORY_CID" "$ENS_DOMAIN"; then
+			exit 0
+		fi
+	fi
+done < <(echo "${RPC_URLS[@]}" | fmt -1 | shuf)
