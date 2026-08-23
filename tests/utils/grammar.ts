1f55785ace3924d1975405e58705fb13a8294ac9
polymutex <polymutex@protonmail.com>
2026-08-23 21:35:50 +0800

    Unignore Harper.js rule "InRealLife".

--- tests/utils/grammar.ts
+++ tests/utils/grammar.ts
@@ -63,7 +63,6 @@ export function isInVocabulary(word: string): boolean {
 }
 
 const TEMPORARILY_IGNORED_LINT_RULES: readonly string[] = [
-	'InRealLife',
 	'InterestedIn',
 	'MassNouns',
 	'MissingDeterminer',
