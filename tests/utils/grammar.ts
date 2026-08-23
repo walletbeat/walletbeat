c94e12f505a574dbf4626eb1da7447cb66d3121c
polymutex <polymutex@protonmail.com>
2026-08-23 22:07:33 +0800

    Unignore Harper.js rule "InterestedIn".

--- tests/utils/grammar.ts
+++ tests/utils/grammar.ts
@@ -63,7 +63,6 @@ export function isInVocabulary(word: string): boolean {
 }
 
 const TEMPORARILY_IGNORED_LINT_RULES: readonly string[] = [
-	'InterestedIn',
 	'MassNouns',
 	'MissingDeterminer',
 	'MissingTo',
