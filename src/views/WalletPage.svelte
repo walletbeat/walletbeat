03b11fb3e2c327b0ec139bd9417b9950fe12adc9
Matt Pereira <mattpereira.dev@gmail.com>
2026-08-24 14:44:00 -0700

    Fix additional Svelte diagnostics

--- src/views/WalletPage.svelte
+++ src/views/WalletPage.svelte
@@ -86,7 +86,7 @@
 
 	// State
 	import { SvelteURLSearchParams } from 'svelte/reactivity'
-	import { isLabeledUrl } from '@/schema/url'
+	import { getUrl } from '@/schema/url'
 	import { IncidentStatus } from '@/types/content/news'
 	import { daysSince } from '@/types/date'
 	import { getNewsForWallet } from '@/data/news'
@@ -135,9 +135,9 @@
 				? decodeURIComponent(currentLink.hash.slice(1))
 				: decodeURIComponent(globalThis.location.hash.slice(1))
 			const currentGroup = (
-				currentId
+				(currentId
 					? globalThis.document.getElementById(currentId)?.closest('.attribute-group')
-					: null
+					: null)
 				?? root.querySelector('.attribute-group')
 			)
 
@@ -495,10 +495,10 @@
 				'@type': 'SoftwareApplication',
 				name: wallet.metadata.displayName,
 				url: (
-					typeof wallet.metadata.url === 'string' ?
-						wallet.metadata.url
+					wallet.metadata.urls?.websites?.[0] !== undefined ?
+						getUrl(wallet.metadata.urls.websites[0])
 					:
-						wallet.metadata.url?.url
+						undefined
 				),
 				applicationCategory: 'Cryptocurrency Wallet',
 				operatingSystem: (
@@ -616,19 +616,21 @@
 				data-column="gap-6"
 			>
 				<nav data-row="gap-2 start wrap">
-					<a
-						href={isLabeledUrl(wallet.metadata.urls?.websites[0]) ? wallet.metadata.urls.websites[0].url : wallet.metadata.urls.websites[0]}
-						data-badge="medium"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						{@html Globe}
-						Website
-					</a>
+					{#if wallet.metadata.urls?.websites?.[0] !== undefined}
+						<a
+							href={getUrl(wallet.metadata.urls.websites[0])}
+							data-badge="medium"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							{@html Globe}
+							Website
+						</a>
+					{/if}
 
 					{#if wallet.metadata.urls?.repositories?.[0] !== undefined}
 						<a
-							href={isLabeledUrl(wallet.metadata.urls.repositories[0]) ? wallet.metadata.urls.repositories[0].url : wallet.metadata.urls.repositories[0]}
+							href={getUrl(wallet.metadata.urls.repositories[0])}
 							data-badge="medium"
 							target="_blank"
 							rel="noopener noreferrer"
