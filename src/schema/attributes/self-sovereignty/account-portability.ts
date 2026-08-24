8a04efe2fa5dc708c4d98dc37da2e9e425496afd
polymutex <polymutex@protonmail.com>
2026-08-25 00:42:43 +0800

    Remove UseEllipsisCharacter lint ignore.

--- src/schema/attributes/self-sovereignty/account-portability.ts
+++ src/schema/attributes/self-sovereignty/account-portability.ts
@@ -247,9 +247,9 @@ function evaluateMultifactor(
 				`),
 				howToImprove: mdParagraph(`
 					{{WALLET_NAME}} should:
-					* ... Either change the smart contract's default control configuration
+					* … Either change the smart contract's default control configuration
 					such that the account is self-custodied by the user from the start
-					* ... Either release an open-source standalone application that allows
+					* … Either release an open-source standalone application that allows
 					users to switch their account to be effectively self-custodied.
 				`),
 			})
