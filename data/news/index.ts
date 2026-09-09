0000ba2c1f49439f89f0ca7f5c77103aa84ee3a0
maykelxyz <michaelpediglorio1109@gmail.com>
2026-09-10 04:59:00 +0800

    News: Add trezor email provider breach

--- data/news/index.ts
+++ data/news/index.ts
@@ -17,6 +17,7 @@ export const allWalletSecurityNews: WalletSecurityNews[] = [
 	(await import('./2026-08-16-safepal-customer-order-data-exposure')).default,
 	(await import('./2026-08-17-bitbox-dixence-firmware-vulnerabilities')).default,
 	(await import('./2026-08-19-rabby-silent-signature-extraction')).default,
+	(await import('./2026-09-10-trezor-email-provider-breach-phishing')).default,
 ].sort((a, b) => b.publishedAt.localeCompare(a.publishedAt))
 
 /**
