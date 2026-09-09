b40f89fd5bb817c62486f5a8587c34b182831fe2
maykelxyz <michaelpediglorio1109@gmail.com>
2026-09-09 18:57:47 +0800

    Data: Update Keycard's basic unlock mechanism

--- data/hardware-wallets/keycard-shell.ts
+++ data/hardware-wallets/keycard-shell.ts
@@ -7,7 +7,7 @@ import {
 } from '@/schema/features/ecosystem/hw-app-connection-support'
 import { HardwarePrivacyType } from '@/schema/features/privacy/hardware-privacy'
 import { HardwareWalletManufactureType, WalletProfile } from '@/schema/features/profile'
-import { BasicUnlockMechanism, DuressAction } from '@/schema/features/security/duress-resistance'
+import { BasicUnlockMechanism, BasicUnlockMechanismSupport, DuressAction } from '@/schema/features/security/duress-resistance'
 import { FirmwareType } from '@/schema/features/security/firmware'
 import {
 	KeyGenerationLocation,
@@ -148,10 +148,10 @@ export const keycardShell: HardwareWallet = {
 						'https://docs.keycard.tech/duress_pin',
 					],
 					mechanisms: {
-						[BasicUnlockMechanism.PIN]: true,
-						[BasicUnlockMechanism.PASSWORD]: false,
-						[BasicUnlockMechanism.BIOMETRIC]: false,
-						[BasicUnlockMechanism.PATTERN]: false,
+						[BasicUnlockMechanism.PIN]: BasicUnlockMechanismSupport.REQUIRED,
+						[BasicUnlockMechanism.PASSWORD]: "NOT_SUPPORTED",
+						[BasicUnlockMechanism.BIOMETRIC]: "NOT_SUPPORTED",
+						[BasicUnlockMechanism.PATTERN]: "NOT_SUPPORTED",
 					},
 				},
 				duressMode: supported({
