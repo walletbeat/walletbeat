c916ad351c744e856f1e5fdd6f2ee370d6775a7c
maykelxyz <michaelpediglorio1109@gmail.com>
2026-09-14 15:46:21 +0800

    Chore: Add gitignore to be ignored in image-integrity

--- tests/image-integrity.test.ts
+++ tests/image-integrity.test.ts
@@ -13,7 +13,7 @@ import { describe, expect, it } from 'vitest'
 import { detectBlockyJpeg } from '@/tools/image-integrity/jpeg-detector-lib'
 import { isSameJson } from '@/utils/json'
 
-import { CodebaseEntryType, crawlCodebase, getRepositoryRoot } from './utils/codebase'
+import { CodebaseEntryType, GitIgnoredFiles, crawlCodebase, getRepositoryRoot } from './utils/codebase'
 
 /**
  * Path to the JSON whitelist of image files already verified as passing every
@@ -680,7 +680,7 @@ describe('image integrity', () => {
 		const trackedFiles: Set<string> = new Set()
 
 		await crawlCodebase({
-			ignore: ['.git', 'node_modules', 'dist', '.cache', '.astro', 'src/generated'],
+			ignore: ['.git', await GitIgnoredFiles(), 'node_modules', 'dist', '.cache', '.astro', 'src/generated'],
 			complexTraversalFn: async (entryBase, getFullEntry) => {
 				if (entryBase.type !== CodebaseEntryType.FILE) {
 					return
