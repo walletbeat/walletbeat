6acfdc9217e5bd0533b50409961866b4e54ca579
polymutex <polymutex@protonmail.com>
2026-09-15 18:56:23 +0800

    Fix lint failures

--- tests/postbuild/vitest.postbuild.config.ts
+++ tests/postbuild/vitest.postbuild.config.ts
@@ -2,7 +2,6 @@ import { getViteConfig } from 'astro/config'
 
 export default getViteConfig({
 	cacheDir: '.cache', // Vitest will append `/vitest` to this path already.
-	// @ts-expect-error - Astro's getViteConfig doesn't expose test config typing
 	test: {
 		environment: 'node',
 		testTimeout: 60000, // 60s
